4efb685402f0c10df4e364e916f2edccc12a8e20
Chaima Hasdi <95375340+Hasdichaima@users.noreply.github.com>
2023-12-19 17:02:38 +0100

    code pour afficher la carte des pays
    Leur distribution par IDH

--- Carto.R
+++ Carto.R
@@ -2,6 +2,7 @@ setwd("C:/Users/user/Documents/GitHub/SpotifyTopSongsGeoStat")
 
 library(ggplot2)
 #library(ggmap)
+library(leaflet)
 library(dplyr)
 
 # Charger les données sur l'indice de développement humain des pays et table des codes iso des pays
@@ -9,31 +10,60 @@ isoCodes <- read.csv('data/codeISOpays.csv', sep=';')
 hdiCountries <- read.csv('data/HDI_data/HDR21-22_Statistical_Annex_HDI.csv', sep=';')
 spotifydb <- read.csv('data/universal_top_spotify_songs/universal_top_spotify_songs_Oct_18_28.csv')
 
+#Calculer la moyenne des tempo par pays
+data_spotify_summary <- spotifydb %>%
+  group_by(country) %>%
+  summarise(
+    moyenne_tempo = mean(tempo, na.rm = TRUE)
+  )
 
 # Fusionner les données de spotify et codeIso 
-spotifyIso <- merge(spotifydb, isoCodes, by.x ="country", by.y ="alpha2", all = TRUE)
+spotifyIso <- merge(data_spotify_summary, isoCodes, by.x ="country", by.y ="alpha2", all = TRUE)
 
 #Resultat de la jointure entre la table spotify et la table des codes ISO
-data_spotify_Iso <- spotifyIso %>% filter(!is.na(spotify_id) & spotify_id != "")
+data_spotify_Iso <- spotifyIso %>% filter(!is.na(moyenne_tempo) & moyenne_tempo != "")
 
 # Fusionner les données de spotify join à code ISO et les données sur l'IDH des pays
 spotifyIsoHDI <- merge(data_spotify_Iso, hdiCountries, by.x = "noms_en_gb", by.y = "Country", all = TRUE)
 
 #Resultat de la jointure entre la table data_spotify_Iso et la table des HDI par pays
-data_spotify_Iso_HDI <- spotifyIsoHDI %>% filter(!is.na(spotify_id) & spotify_id != "")
+data_spotify_Iso_HDI <- spotifyIsoHDI %>% filter(!is.na(moyenne_tempo) & moyenne_tempo != "")
 
 # Sauvegarder le dataframe en tant que fichier CSV
-write.csv(data_spotify_Iso_HDI, file = "spotify_ISO_IDH_Oct.csv", row.names = FALSE)
+write.csv(data_spotify_Iso_HDI, file = "spotify_ISO_IDH_Oct_carto.csv", row.names = FALSE)
 
 # Enlever les enregistrements n'ayant pas de country et idh
 filtered_data <- data_spotify_Iso_HDI %>% filter(!is.na(HDI_value) | !HDI_value == "")
 
+pays <- st_read("data/world-administrative-boundaries/world-administrative-boundaries.shp")
 
-#Calculer la moyenne des tempo par pays
-data_spotify_summary <- filtered_data %>%
-  group_by(alpha3) %>%
-  summarise(
-    moyenne_tempo = mean(tempo, na.rm = TRUE)
+# Effectuer la jointure entre le shapefile et la table des tops chansons
+songs_pays <- merge(pays, filtered_data, by.x = "iso3", by.y = "alpha3", all.x = TRUE)
+
+#C'est le jeu de données qui nous intéresse avec les 72 pays leur idh, le tempo moyen par pays et sa géométrie
+songs_pays_spotify <- songs_pays %>% 
+  filter(!is.na(moyenne_tempo))
+
+# Créer une palette de couleurs pour l'IDH
+palette_couleurs <- colorNumeric(palette = "viridis", domain = songs_pays_spotify$HDI_value)
+
+# Créer la carte Leaflet
+carte <- leaflet() %>%
+  addProviderTiles("CartoDB.Positron") %>% # Style de la carte
+  addPolygons(data = songs_pays_spotify, 
+              fillColor = ~palette_couleurs(HDI_value),
+              fillOpacity = 0.7,
+              color = "white",
+              stroke = TRUE,
+              weight = 1,
+              label = ~noms_en_gb, # Étiquette des pays
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "15px",
+                direction = "auto"
+              ),
+              popup = ~paste("Pays: ", noms_en_gb, "<br>IDH: ", HDI_value)
   )
 
-
+# Afficher la carte
+carte
